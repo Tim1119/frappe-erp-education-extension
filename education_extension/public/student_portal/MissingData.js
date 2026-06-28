63921fa5012837f6a93d22ad97874d2ca7375591
Ajani Timothy <timothyinioluwa@gmail.com>
2026-06-28 19:54:54 +0100

    fix: remove favicon link from index.html to prevent null href error

---
+++ education_extension/public/student_portal/MissingData.js
@@ -0,0 +1,1 @@
+import{o as t,a as s,e as a,t as o}from"./index.js";const c={class:"flex items-center justify-center h-[250px]"},n={class:"text-gray-500"},p={__name:"MissingData",props:{message:{type:String,default:"No data found"}},setup(e){return(r,i)=>(t(),s("div",c,[a("p",n,o(e.message),1)]))}};export{p as _};
